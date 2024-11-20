46bac126e9016b0043a5b198914f543406f41cd1
rgs2151 <rgs2151@columbia.edu>
2024-11-20 00:13:43 -0500

    package

---
+++ out/_next/static/chunks/pages/404-8c380d548b188549.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[336],{4262:(n,_,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return u(3574)}])},3574:(n,_,u)=>{"use strict";u.r(_),u.d(_,{default:()=>r});var e=u(4848);function r(){return(0,e.jsx)("h1",{children:"404 - Page Not Found"})}}},n=>{var _=_=>n(n.s=_);n.O(0,[636,593,792],()=>_(4262)),_N_E=n.O()}]);
